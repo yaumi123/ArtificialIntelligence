cb1697988321c0e5e61116591bf5b211cad47ab3
yaumi123 <feifei@DESKTOP-9PFQEF4.fios-router.home>
2018-05-04 13:19:56 -0400

    finished

---
+++ DealerDiceHMM/src/com/ares/ReadFile.java
@@ -0,0 +1,41 @@
+package com.ares;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFile {
+	public static List<String> readSequence(String path) {
+		try { 
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String line = null; 
+            List<String> dstring = new ArrayList<>();
+
+            while((line=reader.readLine())!=null){ 
+            	if(!line.equals("")) {
+            		if(line.charAt(0)=='[')
+            			dstring.add(line);
+            	}
+            }
+            List<String> dse = new ArrayList<>();
+    		String ds = "";
+    		int seqNum=0;
+    		for(String content : dstring) {
+    			System.out.println("#" + (seqNum+1) + " : " + content);
+    			for(int i=0;i<content.length();i++) {
+    				if(content.charAt(i)!='['&&content.charAt(i)!=','&&content.charAt(i)!=' '&&content.charAt(i)!=']')
+    					ds = ds + String.valueOf(content.charAt(i));
+    			}
+    			dse.add(ds);
+    			ds = "";
+    			seqNum++;
+    		}
+            return dse;
+        } catch (Exception e) { 
+            e.printStackTrace(); 
+        }
+		return null;
+		
+	}
+}
