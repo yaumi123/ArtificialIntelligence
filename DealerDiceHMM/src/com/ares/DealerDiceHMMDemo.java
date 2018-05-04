934bb9c084815a59238269d7edd192a323a10f59
yaumi123 <feifei@DESKTOP-9PFQEF4>
2018-05-04 16:46:42 -0400

    change the calculation function of 'probability' in HMM. Modified the for

--- DealerDiceHMM/src/com/ares/DealerDiceHMMDemo.java
+++ DealerDiceHMM/src/com/ares/DealerDiceHMMDemo.java
@@ -1,53 +1,35 @@
 package com.ares;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
  
- 
 public class DealerDiceHMMDemo {
-	
 	static List<String> diceSequences = new ArrayList<>();
-
-	
 	enum Die
 	{
 		D1,
 		D2,
 		D3,
 	}
-	
 	enum Dice{
 		One,
 		Two,
 		Three,
 	}
 	static int[] states = new int[]{Die.D1.ordinal(), Die.D2.ordinal(),Die.D3.ordinal()};
-
     static int[] observations;
-
     static double[] start_probability = new double[]{0.33, 0.33, 0.33};
-
     static double[][] transititon_probability = new double[][]{
-
             {0.5, 0.25, 0.25},
-
             {0.25, 0.5, 0.25},
-            
             {0.25, 0.25, 0.5},
-
     };
-
     static double[][] emission_probability = new double[][]{
-
             {0.6, 0.2, 0.2},
-
             {0.2, 0.6, 0.2},
             {0.2, 0.2, 0.6},
 
     };
-
 	public static void main(String[] args) {
 		diceSequences = ReadFile.readSequence("res/diceSequences.txt");
 		//System.out.println(diceSequences);
@@ -66,16 +48,12 @@ public class DealerDiceHMMDemo {
 			int[] result = h.viterbi(observations, states, start_probability, transititon_probability, emission_probability);
 			System.out.print("#"+i+" possible sequence : ");
 	        for (int r : result)
-
 	        {
-
 	            System.out.print(Die.values()[r] + " ");
-
 	        }
 	        System.out.print("------Prob : " + h.probability);
 	        System.out.println();
 	        observations = null;
 		}
-		
 	}
 }
